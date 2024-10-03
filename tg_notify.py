047d8922874874ca2407ceea998dce1ab693924f
Kaidstor <kaidstor@gmail.com>
2024-10-03 18:01:12 +0500

    add threadId

--- tg_notify.py
+++ tg_notify.py
@@ -7,10 +7,15 @@ load_environment_variables()
 
 # Получение значения URL
 api_url = os.getenv('API_URL')
+threadId = os.getenv('THREAD_ID')
+
+if not api_url or not threadId:
+    print("Ошибка: Не удалось получить значения URL или threadId из переменных окружения.")
+    exit(1)
 
 def send_telegram_alert(message):
     data = {
-        "threadId": "3500",
+        "threadId": threadId,
         "type": "warning",
         "text": message
     }
